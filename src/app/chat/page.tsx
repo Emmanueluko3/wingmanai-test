40221a44d2f62a3659e4ebc49cf881715d10a39d
Emmanueluko3 <emmanueluko90@gmail.com>
2025-01-11 16:54:04 +0100

    Fixes

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-export default function Dashboard({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function Chat() {
   return (
     <div className="flex justify-center items-start bg-white text-gray-800 w-8/12 min-h-96 border p-6 rounded-3xl">
       <h2 className="text-primaryDark text-3xl font-bold">WingmanAI</h2>
