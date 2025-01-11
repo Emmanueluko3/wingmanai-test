2a04e03bd0c30cc822f40985645bcfbd9ab5ee33
Emmanueluko3 <emmanueluko90@gmail.com>
2025-01-11 17:21:15 +0100

    Fixes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { CgSpinner } from "react-icons/cg";
 
 export default function Dashboard() {
   const router = useRouter();
